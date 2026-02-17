2c98d5eab56459c57437e86863b6e564c79810c3
Aditya Kadu <117461241+Aditya986025@users.noreply.github.com>
2026-02-17 13:34:03 +0530

    Update backend.groovy

--- flight-reservation-app-main/FlightReservationApplication/backend.groovy
+++ flight-reservation-app-main/FlightReservationApplication/backend.groovy
@@ -1,47 +1,53 @@
-pipeline{
-    agent any 
-    stages{
-        stage('Code-pull'){
-            steps{
-                git branch: 'master', url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Code-pull') {
+            steps {
+                git branch: 'master',
+                    url: 'https://github.com/mayurmwagh/flight-reservation-app.git'
             }
         }
-        stage('Build'){
-            steps{
+
+        stage('Build') {
+            steps {
                 sh '''
-                    cd FlightReservationApplication
+                    cd flight-reservation-app-main/FlightReservationApplication
                     mvn clean package
                 '''
             }
         }
-        stage('QA-Test'){
-            steps{
+
+        stage('QA-Test') {
+            steps {
                 withSonarQubeEnv(installationName: 'sonar', credentialsId: 'sonar-token') {
-                  sh'''
-                     cd FlightReservationApplication
-                     mvn sonar:sonar -Dsonar.projectKey=flight-reservation   
-                  '''
+                    sh '''
+                        cd flight-reservation-app-main/FlightReservationApplication
+                        mvn sonar:sonar -Dsonar.projectKey=flight-reservation
+                    '''
                 }
             }
         }
-        stage('Docker-build'){
-            steps{
-                sh'''
-                    cd FlightReservationApplication
-                    docker build -t aditya986025/flight-reservation-15-16:latest . 
-                    docker push  aditya986025/flight-reservation-15-16:latest
+
+        stage('Docker-build') {
+            steps {
+                sh '''
+                    cd flight-reservation-app-main/FlightReservationApplication
+                    docker build -t aditya986025/flight-reservation-15-16:latest .
+                    docker push aditya986025/flight-reservation-15-16:latest
                     docker rmi aditya986025/flight-reservation-15-16:latest
                 '''
             }
         }
-        stage('Deploy'){
-            steps{
-                sh'''
-                    cd FlightReservationApplication   
+
+        stage('Deploy') {
+            steps {
+                sh '''
+                    cd flight-reservation-app-main/FlightReservationApplication
                     kubectl apply -f k8s/
                 '''
             }
         }
-
     }
 }
